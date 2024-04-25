67aa82d748ddc3177a12c1f719fe9e5613b7933c
dimitrijjedich <dimitrij@startup-werk.de>
2024-04-25 09:34:49 +0200

    add content to servers component HTML

--- apps/my-first-app/src/app/servers/servers.component.ts
+++ apps/my-first-app/src/app/servers/servers.component.ts
@@ -6,7 +6,7 @@ import { ServerComponent } from '../server/server.component';
   selector: 'app-servers',
   standalone: true,
   imports: [CommonModule, ServerComponent],
-  template: '<app-server></app-server><app-server></app-server>',
+  template: '<h4>I\'m in the ServersComponent</h4><app-server></app-server><app-server></app-server>',
   styleUrl: './servers.component.css',
 })
 export class ServersComponent {}
