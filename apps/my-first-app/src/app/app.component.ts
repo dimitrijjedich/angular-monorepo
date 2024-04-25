b8ad998e3d97e79cac0d0ccef1f3f26543504896
dimitrijjedich <dimitrij@startup-werk.de>
2024-04-25 09:16:40 +0200

    add server-component to app component

--- apps/my-first-app/src/app/app.component.ts
+++ apps/my-first-app/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ServerComponent } from './server/server.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, ServerComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
