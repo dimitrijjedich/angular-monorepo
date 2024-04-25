8a90a4f246026e754aa71c38a086ce97c2e86c46
dimitrijjedich <dimitrij@startup-werk.de>
2024-04-25 08:12:21 +0200

    cleanup imports

--- apps/my-first-app/src/app/app.component.ts
+++ apps/my-first-app/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, ReactiveFormsModule],
+  imports: [RouterModule, ReactiveFormsModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
