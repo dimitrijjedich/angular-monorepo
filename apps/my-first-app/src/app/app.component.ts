d4d897e455f730af16edaa707c1070b9b8511fb1
dimitrijjedich <dimitrij@startup-werk.de>
2024-04-25 18:11:31 +0200

    add new components to app component

--- apps/my-first-app/src/app/app.component.ts
+++ apps/my-first-app/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, ServersComponent],
+  imports: [RouterModule, ServersComponent, SuccessAlertComponent, WarningAlertComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
