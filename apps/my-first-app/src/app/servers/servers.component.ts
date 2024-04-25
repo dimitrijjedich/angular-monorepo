b36ce9e36ac6d236dbeb81aeeeb9e4c0804a542f
dimitrijjedich <dimitrij@startup-werk.de>
2024-04-25 09:28:35 +0200

    replace usage of templateUrl with template and HTML in components TS file

--- apps/my-first-app/src/app/servers/servers.component.ts
+++ apps/my-first-app/src/app/servers/servers.component.ts
@@ -6,7 +6,7 @@ import { ServerComponent } from '../server/server.component';
   selector: 'app-servers',
   standalone: true,
   imports: [CommonModule, ServerComponent],
-  templateUrl: './servers.component.html',
+  template: '<app-server></app-server><app-server></app-server>',
   styleUrl: './servers.component.css',
 })
 export class ServersComponent {}
