756dca5f196b70f9806d42cc0a50ee6b53ff1981
dimitrijjedich <dimitrij@startup-werk.de>
2024-04-25 09:23:23 +0200

    nest server component into servers component

--- apps/my-first-app/src/app/servers/servers.component.ts
+++ apps/my-first-app/src/app/servers/servers.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ServerComponent } from '../server/server.component';
 
 @Component({
   selector: 'app-servers',
   standalone: true,
-  imports: [CommonModule],
+    imports: [CommonModule, ServerComponent],
   templateUrl: './servers.component.html',
   styleUrl: './servers.component.css',
 })
