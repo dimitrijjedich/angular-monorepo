6dcddea8f71f04f6c2072452f2b1ca6fd67e7db3
dimitrijjedich <dimitrij@startup-werk.de>
2024-04-25 09:07:16 +0200

    create server component
    nx g @nx/angular:component server --directory=apps/my-first-app/src/app/server --standalone --export

---
+++ apps/my-first-app/src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-server',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './server.component.html',
+  styleUrl: './server.component.css',
+})
+export class ServerComponent {}
