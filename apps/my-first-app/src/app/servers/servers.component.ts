8c296f435d40295578c6b7315aaf394a921ef276
dimitrijjedich <dimitrij@startup-werk.de>
2024-04-30 09:11:01 +0200

    add example for string interpolations

--- apps/my-first-app/src/app/servers/servers.component.ts
+++ apps/my-first-app/src/app/servers/servers.component.ts
@@ -9,4 +9,11 @@ import { ServerComponent } from '../server/server.component';
   templateUrl: './servers.component.html',
   styles: ['h4 { color: red;}'],
 })
-export class ServersComponent {}
+export class ServersComponent {
+  serverId: number = 10;
+  serverStatus: string = 'offline';
+
+  getServerStatus() {
+    return this.serverStatus
+  }
+}
